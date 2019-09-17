6354a86b9fbb3a4b0c597db70c90e06661fe025e
Fortune <fortunevalitova@yahoo.com>
2019-09-17 17:09:12 -0400

    Resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
@@ -6,38 +5,28 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material';
 import { IconsModule } from 'angular-bootstrap-md';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
+
 //Modules
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AngularMaterialModule} from './angular-material/angular-material.module';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {RoutingModule} from './routing/routing.module';
 import {HttpClientModule} from '@angular/common/http';
 
 //Services
 import {ColumnsService} from './service/columns.service';
->>>>>>> 7e5962974e686abb5fd86de0beca1824476e326b
 
 //Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-<<<<<<< HEAD
+
 
 import { AngularMaterialModule} from './angular-material/angular-material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RoutingModule } from './routing/routing.module';
-import { FormulaComponent } from './components/formula/formula.component';
 import { ResourceComponent } from './components/resource/resource.component';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-=======
+
 import { FormulaComponent } from './components/formula/formula.component';
 import { FormulaFieldsComponent } from './components/formula-fields/formula-fields.component';
 import { CellsService } from './service/cells.service';
-
->>>>>>> 7e5962974e686abb5fd86de0beca1824476e326b
 
 @NgModule({
   declarations: [
@@ -45,11 +34,8 @@ import { CellsService } from './service/cells.service';
     HeaderComponent,
     NavbarComponent,
     FormulaComponent,
-<<<<<<< HEAD
-    ResourceComponent
-=======
+    ResourceComponent,
     FormulaFieldsComponent,
->>>>>>> 7e5962974e686abb5fd86de0beca1824476e326b
   ],
   imports: [
     BrowserModule,
